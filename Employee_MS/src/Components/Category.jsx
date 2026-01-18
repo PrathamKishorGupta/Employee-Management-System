a000157f64a0a463293d5ec0b8dd5b1b696ff00b
Pratham Kishor Gupta <prathamkishorgupta@gmail.com>
2026-01-18 16:23:12 +0530

    Update API endpoint for fetching categories

--- Employee_MS/src/Components/Category.jsx
+++ Employee_MS/src/Components/Category.jsx
@@ -9,7 +9,7 @@ const Category = () => {
 
   useEffect(() => {
     console.log('Fetching categories...');
-    axios.get('http://localhost:3000/auth/categories', { withCredentials: true })
+    axios.get('https://employee-management-system-mk0o.onrender.com/auth/categories', { withCredentials: true })
       .then(result => {
         console.log('Categories response:', result.data);
         // Handle both array response and wrapped response
